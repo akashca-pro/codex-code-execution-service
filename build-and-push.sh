b9c9422bdc7cf0c84dfdc4fe0fb11cb774d79248
akashca-pro <akashca656@gmail.com>
2025-11-15 12:08:11 +0530

    fix : add filename option for docker build and push command.

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # --- 1. Argument Check ---
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "❌ Error: You must provide BOTH an image name and a version tag."
-  echo "Usage: $0 <image-name> <version>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo "❌ Error: You must provide BOTH an image name, dockerfile path and a version tag."
+  echo "Usage: $0 <file-name> <image-name> <version>"
   exit 1
 fi
 
@@ -11,16 +11,18 @@ fi
 IMAGE_NAME=$1
 VERSION=$2
 FULL_TAG="$IMAGE_NAME:$VERSION"
+FILE_NAME=$3
 
 echo "==========================================="
 echo "  Starting Docker Build and Push"
 echo "  Image Name: $IMAGE_NAME"
 echo "  Version Tag: $VERSION"
+echo " File name: $FILE_NAME "
 echo "==========================================="
 
 # --- 3. Build the Docker Image ---
 echo "⚙️  Building Docker image: $FULL_TAG"
-docker build --no-cache -t "$FULL_TAG" .
+docker build --no-cache -f $FILE_NAME -t "$FULL_TAG" .
 
 # Check if the build was successful
 if [ $? -ne 0 ]; then
